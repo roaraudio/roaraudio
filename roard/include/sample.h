4212073b51b87ee800bf071ef9d93e41644a14aa
phi <phi>
2008-06-22 14:14:20 +0000

    added some protptypes for samples

--- roard/include/sample.h
+++ roard/include/sample.h
@@ -5,6 +5,16 @@
 
 #include <roaraudio.h>
 
+#define ROAR_SAMPLES_MAX  32
+
+struct roar_sample * g_samples[ROAR_SAMPLES_MAX];
+
+int samples_init (void);
+int samples_free      (void);
+int samples_new       (void);
+int samples_delete    (int id);
+int samples_set_name  (int id, char * name);
+
 #endif
 
 //ll
