c243b9b76406c778a8ffde90af1996da2960643c
phi <phi>
2008-06-22 01:01:40 +0000

    added some prototypes

--- include/libroardsp/poly.h
+++ include/libroardsp/poly.h
@@ -5,6 +5,14 @@
 
 #include "libroar.h"
 
+int roar_math_mkpoly (float * poly, float * data, int len);
+
+int roar_math_mkpoly_2x2 (float * poly, float * data);
+int roar_math_mkpoly_3x3 (float * poly, float * data);
+int roar_math_mkpoly_4x4 (float * poly, float * data);
+int roar_math_mkpoly_5x5 (float * poly, float * data);
+
+
 #endif
 
 //ll
