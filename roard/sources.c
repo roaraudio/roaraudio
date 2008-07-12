33b3187db03171977a2b8855eb4b3a65c90a0688
phi <phi>
2008-07-12 11:11:05 +0000

    done some coding to support sources

--- roard/sources.c
+++ roard/sources.c
@@ -20,8 +20,39 @@ int sources_free (void) {
  return 0;
 }
 
-int sources_add (char * driver, char * device, char * containe, char * options, int primary) {
+int sources_add (char * driver, char * device, char * container, char * options, int primary) {
+ if ( strcmp(driver, "raw") == 0 )
+  return sources_add_raw(driver, device, container, options, primary);
+
  return -1;
+}
+
+int sources_add_raw (char * driver, char * device, char * container, char * options, int primary) {
+ int stream;
+ int fh;
+ struct roar_stream * s;
+
+ if ( (fh = open(device, O_RDONLY, 0644)) == -1 ) {
+  return -1;
+ }
+
+ if ( (stream = streams_new()) == -1 ) {
+  close(fh);
+  return -1;
+ }
+
+ streams_get(stream, (struct roar_stream_server **)&s);
+
+ memcpy(&(s->info), g_sa, sizeof(struct roar_audio_info));
+
+ s->dir        = ROAR_DIR_PLAY;
+ s->pos_rel_id = -1;
+
+ streams_set_fh(stream, fh);
+
+ client_stream_add(g_source_client, stream);
+
+ return 0;
 }
 
 //ll
