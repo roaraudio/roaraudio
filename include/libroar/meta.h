3981c264d97de31a333d5e89ac7fcc3f6dc3ae19
phi <phi>
2008-07-13 00:21:17 +0000

    added roar_stream_meta_get() and roar_meta_free()

--- include/libroar/meta.h
+++ include/libroar/meta.h
@@ -6,6 +6,9 @@
 #include "libroar.h"
 
 int roar_stream_meta_set (struct roar_connection * con, struct roar_stream * s, int mode, struct roar_meta * meta);
+int roar_stream_meta_get (struct roar_connection * con, struct roar_stream * s, struct roar_meta * meta);
+
+int roar_meta_free (struct roar_meta * meta);
 
 #endif
 
