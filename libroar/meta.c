19213b310d8e98d157f09ba85636be4b368854bc
phi <phi>
2008-07-13 00:20:52 +0000

    added roar_stream_meta_get()

--- libroar/meta.c
+++ libroar/meta.c
@@ -35,4 +35,44 @@ int roar_stream_meta_set (struct roar_connection * con, struct roar_stream * s, 
  return -1;
 }
 
+int roar_stream_meta_get (struct roar_connection * con, struct roar_stream * s, struct roar_meta * meta) {
+ struct roar_message m;
+ char * c;
+
+ m.cmd     = ROAR_CMD_SET_META;
+ m.stream  = s->id;
+// m.datalen = len;
+
+ m.data[0] = 0;
+ m.data[1] = meta->type;
+ m.datalen = 2;
+
+ if ( m.cmd != ROAR_CMD_OK )
+  return -1;
+
+ if ( m.datalen < 2 )
+  return -1;
+
+ if ( m.data[0] != 0 )
+  return -1;
+
+ if ( (c = malloc(((unsigned) m.data[1]) + 1)) == NULL )
+  return -1;
+
+ strncpy(c, &(m.data[2]), (unsigned) m.data[1]);
+ c[(unsigned) m.data[1]] = 0;
+
+ meta->value  = c;
+ meta->key[0] = 0;
+
+ return 0;
+}
+
+int roar_meta_free (struct roar_meta * meta) {
+ if ( meta->value )
+  free(meta->value);
+
+ return 0;
+}
+
 //ll
