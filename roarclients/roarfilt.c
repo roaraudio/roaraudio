37842593338e151277484b11c7b4125e824ccd3b
phi <phi>
2008-07-13 17:23:13 +0000

    added esdfilt like --half --half and --double

--- roarclients/roarfilt.c
+++ roarclients/roarfilt.c
@@ -14,8 +14,31 @@ void usage (void) {
         "  --bits   BITS      - Set bits per sample\n"
         "  --chans  CHANNELS  - Set number of channels\n"
         "  --help             - Show this help\n"
+        "\n"
+        "  --half             - half the volume\n"
+        "  --double           - double the volume\n"
        );
 
+}
+
+void vol2 (void * data, int mul, int div, int len) {
+ int16_t * samples = (int16_t *) data;
+ int i;
+
+ len /= 2;
+
+ for (i = 0; i < len; i++)
+  samples[i] = ((int) samples[i] * mul) / div;
+}
+
+void vol1 (void * data, int mul, int div, int len) {
+ int8_t * samples = (int8_t *) data;
+ int i;
+
+ len /= 2;
+
+ for (i = 0; i < len; i++)
+  samples[i] = ((int) samples[i] * mul) / div;
 }
 
 int main (int argc, char * argv[]) {
@@ -27,6 +50,7 @@ int main (int argc, char * argv[]) {
  char * k;
  int    fh;
  int    i;
+ int    mul = 1, div = 1;
  char buf[BUFSIZE];
 
  for (i = 1; i < argc; i++) {
@@ -40,6 +64,10 @@ int main (int argc, char * argv[]) {
    bits = atoi(argv[++i]);
   } else if ( strcmp(k, "--channels") == 0 || strcmp(k, "--chans") == 0 ) {
    channels = atoi(argv[++i]);
+  } else if ( strcmp(k, "--half") == 0 ) {
+   div *= 2;
+  } else if ( strcmp(k, "--double") == 0 ) {
+   mul *= 2;
   } else if ( strcmp(k, "--help") == 0 ) {
    usage();
    return 0;
@@ -55,9 +83,27 @@ int main (int argc, char * argv[]) {
   return 1;
  }
 
- while((i = read(fh, buf, BUFSIZE)))
-  if (write(fh, buf, i) != i)
-   break;
+ if ( mul == div ) {
+  fprintf(stderr, "Error: filter is useless!\n");
+  return 0;
+ }
+
+ if ( bits == 16 ) {
+  while((i = read(fh, buf, BUFSIZE))) {
+   vol2((void*)buf, mul, div, i);
+   if (write(fh, buf, i) != i)
+    break;
+  }
+ } else if ( bits == 8 ) {
+  while((i = read(fh, buf, BUFSIZE))) {
+   vol1((void*)buf, mul, div, i);
+   if (write(fh, buf, i) != i)
+    break;
+  }
+ } else {
+  fprintf(stderr, "Error: %i bits per sample is not supported!\n", bits);
+  return 1;
+ }
 
  roar_simple_close(fh);
 
