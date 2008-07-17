1fd40278ba135151fec6c1911d19f2074fcdc8c2
phi <phi>
2008-07-17 21:24:49 +0000

    added --libs and --cflags

--- roarclients/roar-config.c
+++ roarclients/roar-config.c
@@ -1,6 +1,27 @@
 //roar-config.c:
 
+#include <roaraudio.h>
+
 int main (int argc, char * argv[]) {
+ int i;
+
+ if ( argc == 1 ) {
+  printf("Usage: roar-config [--version] [--libs] [--cflags]\n");
+  return 0;
+ }
+
+ for (i = 1; i < argc; i++) {
+  if ( !strcmp(argv[i], "--version") ) {
+  } else if ( !strcmp(argv[i], "--libs") ) {
+   printf("%s\n", ROAR_LIBS);
+  } else if ( !strcmp(argv[i], "--cflags") ) {
+   printf("%s\n", ROAR_CFLAGS);
+  } else {
+   fprintf(stderr, "Unknown option: %s\n", argv[i]);
+   return 1;
+  }
+ }
+
  return 0;
 }
 
