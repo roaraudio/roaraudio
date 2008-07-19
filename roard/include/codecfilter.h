4581dd61c7130145b416ee2ed59257a5e5cb1012
phi <phi>
2008-07-19 22:49:40 +0000

    added basic prototypes, mainly stolen from driver.h

--- roard/include/codecfilter.h
+++ roard/include/codecfilter.h
@@ -5,6 +5,33 @@
 
 #include <roaraudio.h>
 
+#define CODECFILTER_USERDATA_T void *
+
+
+struct roar_codecfilter {
+ char * name;
+ char * desc;
+ char * devices;
+ int (*open )(CODECFILTER_USERDATA_T * inst, char * device, struct roar_stream_server * info);
+ int (*close)(CODECFILTER_USERDATA_T   inst);
+ int (*pause)(CODECFILTER_USERDATA_T   inst, int newstate);
+ int (*write)(CODECFILTER_USERDATA_T   inst, char * buf, int len);
+ int (*read )(CODECFILTER_USERDATA_T   inst, char * buf, int len);
+ int (*flush)(CODECFILTER_USERDATA_T   inst);
+};
+
+void print_codecfilterlist (void);
+
+int codecfilter_open (CODECFILTER_USERDATA_T * inst,
+                 int * codecfilter_id, char * codecfilter /* NOTE: this is not part of struct roar_codecfilter's def! */,
+                 char * device, struct roar_stream_server * info);
+int codecfilter_close(CODECFILTER_USERDATA_T   inst, int codecfilter);
+int codecfilter_pause(CODECFILTER_USERDATA_T   inst, int codecfilter, int newstate);
+int codecfilter_write(CODECFILTER_USERDATA_T   inst, int codecfilter, char * buf, int len);
+int codecfilter_read (CODECFILTER_USERDATA_T   inst, int codecfilter, char * buf, int len);
+int codecfilter_flush(CODECFILTER_USERDATA_T   inst, int codecfilter);
+
+
 #endif
 
 //ll
