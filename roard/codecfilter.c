3d9aa050eaafede347f763324460df57bc3a4478
phi <phi>
2008-07-19 23:02:00 +0000

    added null filter and end of list

--- roard/codecfilter.c
+++ roard/codecfilter.c
@@ -2,4 +2,9 @@
 
 #include "roard.h"
 
+struct roar_codecfilter g_codecfilter[] = {
+ {"null", "null codec filter", NULL, NULL, NULL, NULL, NULL, NULL},
+ {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL} // end of list
+};
+
 //ll
