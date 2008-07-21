956114df85bae32f90745eaf5255f5ee0111e2a6
phi <phi>
2008-07-21 11:08:11 +0000

    added roar_file_send_raw()

--- include/libroar/file.h
+++ include/libroar/file.h
@@ -5,6 +5,12 @@
 
 #include "libroar.h"
 
+#ifdef ROAR_HAVE_LINUX_SENDFILE
+#include <sys/sendfile.h>
+#endif
+
+ssize_t roar_file_send_raw (int out, int in);
+
 #endif
 
 //ll
--- libroar/file.c
+++ libroar/file.c
@@ -2,4 +2,26 @@
 
 #include "libroar.h"
 
+#define BUFSIZE 8192
+#define BUFMAX  65536
+
+ssize_t roar_file_send_raw (int out, int in) {
+ ssize_t r = 0;
+ ssize_t ret;
+ int len;
+ char buf[BUFSIZE];
+
+#ifdef ROAR_HAVE_LINUX_SENDFILE
+ while ((ret = sendfile(out, in, NULL, BUFMAX)) > 0)
+  r += ret;
+#endif
+
+ // TODO: try mmap here!
+
+ while ((len = read(in, buf, BUFSIZE)) > 0)
+  r += write(out, buf, len);
+
+ return r;
+}
+
 //ll
