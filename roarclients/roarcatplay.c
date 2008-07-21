5f9b431050e3363e65d5c2e71ab20723156c3fe3
phi <phi>
2008-07-21 11:39:05 +0000

    added roarcatplay

---
+++ roarclients/roarcatplay.c
@@ -0,0 +1,81 @@
+//roarcat.c:
+
+#include <roaraudio.h>
+
+#define BUFSIZE 1024
+
+void usage (void) {
+ printf("roarcatplay [OPTIONS]... [FILE]\n");
+
+ printf("\nOptions:\n\n");
+
+ printf("  --server SERVER    - Set server hostname\n"
+        "  --rate   RATE      - Set sample rate\n"
+        "  --bits   BITS      - Set bits per sample\n"
+        "  --chans  CHANNELS  - Set number of channels\n"
+        "  --codec  CODEC     - Set the codec\n"
+        "  --help             - Show this help\n"
+       );
+
+}
+
+int main (int argc, char * argv[]) {
+ int    rate     = 44100;
+ int    bits     = 16;
+ int    channels = 2;
+ int    codec    = ROAR_CODEC_DEFAULT;
+ char * server   = NULL;
+ char * k;
+ int    fh;
+ int    i;
+ int    in = -1;
+ char buf[BUFSIZE];
+
+ for (i = 1; i < argc; i++) {
+  k = argv[i];
+
+  if ( strcmp(k, "--server") == 0 ) {
+   server = argv[++i];
+  } else if ( strcmp(k, "--rate") == 0 ) {
+   rate = atoi(argv[++i]);
+  } else if ( strcmp(k, "--bits") == 0 ) {
+   bits = atoi(argv[++i]);
+  } else if ( strcmp(k, "--channels") == 0 || strcmp(k, "--chans") == 0 ) {
+   channels = atoi(argv[++i]);
+  } else if ( strcmp(k, "--codec") == 0 ) {
+   codec = atoi(argv[++i]);
+  } else if ( strcmp(k, "--help") == 0 ) {
+   usage();
+   return 0;
+  } else if ( in == -1 ) {
+   if ( (in = open(k, O_RDONLY, 0644)) == -1 ) {
+    fprintf(stderr, "Error: can not open file: %s: %s\n", k, strerror(errno));
+    return 1;
+   }
+  } else {
+   fprintf(stderr, "Error: unknown argument: %s\n", k);
+   usage();
+   return 1;
+  }
+ }
+
+ if ( (fh = roar_simple_play(rate, channels, bits, codec, server, "roarcat")) == -1 ) {
+  fprintf(stderr, "Error: can not start playback\n");
+  return 1;
+ }
+
+ if ( in == -1 )
+  in = ROAR_STDIN;
+
+ while((i = read(in, buf, BUFSIZE)))
+  if (write(fh, buf, i) != i)
+   break;
+
+ roar_simple_close(fh);
+
+ close(in);
+
+ return 0;
+}
+
+//ll
