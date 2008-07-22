aa5c7470943bf3b844831522933e69419bc7824e
phi <phi>
2008-07-22 20:16:50 +0000

    added a newline at program exit

--- roarclients/roarvumeter.c
+++ roarclients/roarvumeter.c
@@ -105,6 +105,8 @@ int main (int argc, char * argv[]) {
 
  vumeter(fh, samples*channels, bits, channels);
 
+ printf("\n"); // if the reach this then roard has quited and we should print a newline
+
  roar_simple_close(fh);
 
  return 0;
