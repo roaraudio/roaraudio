e3b15e64fbfdbe61d89cdcee67ad621faebe8755
phi <phi>
2008-07-24 14:12:05 +0000

    added support for esd_play_file() but without fallback

--- libroaresd/esdfile.c
+++ libroaresd/esdfile.c
@@ -9,7 +9,8 @@ int esd_send_file( int esd, AFfilehandle au_file, int frame_length ) {
  return -1;
 }
 int esd_play_file( const char *name_prefix, const char *filename, int fallback ) {
- return -1;
+ // TODO: add support for fallback
+ return roar_simple_play_file((char*)filename, NULL, (char*)name_prefix) == -1 ? -1 : 0;
 }
 int esd_file_cache( int esd, const char *name_prefix, const char *filename ) {
  return -1;
