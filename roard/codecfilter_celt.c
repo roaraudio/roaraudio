984859d07e8ddbbde7d3caf48f12286a35bb44ac
phi <phi>
2008-07-25 00:32:09 +0000

    done some basic stuff for CELT

--- roard/codecfilter_celt.c
+++ roard/codecfilter_celt.c
@@ -6,11 +6,46 @@ int cf_celt_open(CODECFILTER_USERDATA_T * inst, int codec,
                                             struct roar_stream_server * info,
                                             struct roar_codecfilter   * filter) {
  struct codecfilter_celt_inst * self = malloc(sizeof(struct codecfilter_celt_inst));
+ struct roar_stream * s = (struct roar_stream *) info;
 
  if ( !self )
   return -1;
 
+/*
+ CELTMode * mode;
+ CELTEncoder * encoder;
+ CELTDecoder * decoder;
+ int frame_size;
+ int lookahead;
+ int out_size;
+ char * ibuf;
+ char * obuf;
+ char * rest;
+ int s_buf;
+ int f_rest; /-* how much is in rest? *-/
+*/
+
  self->stream               = info;
+ self->frame_size           = 256;
+ self->lookahead            = self->frame_size;
+ self->mode                 = celt_mode_create(s->info.rate, s->info.channels, self->frame_size, self->lookahead, NULL);
+ self->encoder              = NULL;
+ self->decoder              = NULL;
+
+ if ( !self->mode ) {
+  free(self);
+  return -1;
+ }
+
+ if ( s->dir == ROAR_DIR_PLAY ) {
+   self->decoder = celt_decoder_create(self->mode);
+ } else if ( s->dir == ROAR_DIR_MONITOR ) {
+   self->encoder = celt_encoder_create(self->mode);
+ } else {
+  celt_mode_destroy(self->mode);
+  free(self);
+  return -1;
+ }
 
  *inst = (CODECFILTER_USERDATA_T) self;
 
@@ -24,6 +59,15 @@ int cf_celt_close(CODECFILTER_USERDATA_T   inst) {
 
  if ( !inst )
   return -1;
+
+ if ( self->encoder )
+ celt_encoder_destroy(self->encoder);
+
+ if ( self->decoder )
+ celt_decoder_destroy(self->decoder);
+
+ if ( self->mode )
+  celt_mode_destroy(self->mode);
 
  free(inst);
  return 0;
