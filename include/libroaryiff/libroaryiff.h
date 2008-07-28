baf0c33bde3f2e6110f8a784e819c4f447680cb1
phi <phi>
2008-07-28 21:53:14 +0000

    added some needed basic header files :)

--- include/libroaryiff/libroaryiff.h
+++ include/libroaryiff/libroaryiff.h
@@ -6,6 +6,9 @@
 #include <Y2/Y.h>           /* Basic Y types and constants. */
 #include <Y2/Ylib.h>        /* YLib functions and structs. */
 
+#include <unistd.h>
+#include <string.h>
+
 #endif
 
 //ll
