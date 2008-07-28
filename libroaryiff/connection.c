0714d6570787b53a174fdae899c28e43ebdbb55b
phi <phi>
2008-07-28 21:52:52 +0000

    done some basic code

--- libroaryiff/connection.c
+++ libroaryiff/connection.c
@@ -2,4 +2,18 @@
 
 #include <libroaryiff.h>
 
+YConnection *YOpenConnection (const char *start_arg, const char *con_arg) {
+ YConnection * con = malloc(sizeof(YConnection));
+
+ memset(con, 0, sizeof(YConnection));
+
+ return con;
+}
+
+void YCloseConnection (YConnection *connection, Boolean no_shutdown) {
+ if ( !connection )
+  return;
+
+}
+
 //ll
