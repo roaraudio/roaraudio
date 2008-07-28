425d33e2f4dfe795d84dae994a14bba6d97533b2
phi <phi>
2008-07-28 21:40:38 +0000

    added libroaryiff.h

---
+++ include/libroaryiff/libroaryiff.h
@@ -0,0 +1,11 @@
+//libroaryiff.h:
+
+#ifndef _LIBROARYIFF_H_
+#define _LIBROARYIFF_H_
+
+#include <Y2/Y.h>           /* Basic Y types and constants. */
+#include <Y2/Ylib.h>        /* YLib functions and structs. */
+
+#endif
+
+//ll
