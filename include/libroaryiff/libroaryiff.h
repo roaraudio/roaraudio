c331268a7c6f93ab68bf3a67eda4eb3b08331d85
phi <phi>
2008-07-29 20:22:19 +0000

    adding basic macros for YID<->Roar conv and include roaraudio.h :)

--- include/libroaryiff/libroaryiff.h
+++ include/libroaryiff/libroaryiff.h
@@ -3,11 +3,18 @@
 #ifndef _LIBROARYIFF_H_
 #define _LIBROARYIFF_H_
 
+#include <roaraudio.h>
+
 #include <Y2/Y.h>           /* Basic Y types and constants. */
 #include <Y2/Ylib.h>        /* YLib functions and structs. */
 
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+
+// YID 0 is error like -1 on libroar!
+#define ROARYIFF_YID2ROAR(x) ((x)-1)
+#define ROARYIFF_ROAR2YID(x) ((x)+1)
 
 #endif
 
