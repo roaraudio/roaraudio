1f7af66665e243c2cb10ec1fae4f0d5f5a713a57
phi <phi>
2008-07-29 22:39:36 +0000

    added roar_acl_rule_chk_connect()

--- include/libroar/acl.h
+++ include/libroar/acl.h
@@ -5,6 +5,7 @@
 
 #include "libroar.h"
 
+int roar_acl_rule_chk_connect (struct roar_acl_rule * rule, struct sockaddr * addr, socklen_t addrlen);
 
 #endif
 
--- libroar/acl.c
+++ libroar/acl.c
@@ -2,4 +2,18 @@
 
 #include "libroar.h"
 
+int roar_acl_rule_chk_connect (struct roar_acl_rule * rule, struct sockaddr * addr, socklen_t addrlen) {
+
+ if ( !rule )
+  return -1;
+
+ if ( rule->pid || rule->uid || rule->gid || rule->cookie )
+  return 0;
+
+ if ( rule->addr && !addr )
+  return 0;
+
+ return -1;
+}
+
 //ll
