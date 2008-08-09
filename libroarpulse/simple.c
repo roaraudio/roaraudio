fd9b9ed920e0eb13e0f9ed8d44e6111f29db963c
phi <phi>
2008-08-09 23:05:39 +0000

    added read(), write(), fsync()

--- libroarpulse/simple.c
+++ libroarpulse/simple.c
@@ -19,31 +19,56 @@ pa_simple* pa_simple_new(
 
 /** Close and free the connection to the server. The connection objects becomes invalid when this is called. */
 void pa_simple_free(pa_simple *s) {
+ struct roarpulse_simple * ss = (struct roarpulse_simple*) s;
+ if ( !s )
+  return;
+
+ free(s);
 }
 
 /** Write some data to the server */
 int pa_simple_write(pa_simple *s, const void*data, size_t length, int *error) {
- return -1;
+ struct roarpulse_simple * ss = (struct roarpulse_simple*) s;
+ if ( !s )
+  return -1;
+
+ return read(ss->data_fh, data, length);
 }
 
 /** Wait until all data already written is played by the daemon */
 int pa_simple_drain(pa_simple *s, int *error) {
+ struct roarpulse_simple * ss = (struct roarpulse_simple*) s;
+ if ( !s )
+  return -1;
+
  return -1;
 }
 
 /** Read some data from the server */
 int pa_simple_read(pa_simple *s, void*data, size_t length, int *error) {
- return -1;
+ struct roarpulse_simple * ss = (struct roarpulse_simple*) s;
+ if ( !s )
+  return -1;
+
+ return read(ss->data_fh, data, legth);
 }
 
 /** Return the playback latency. \since 0.5 */
 pa_usec_t pa_simple_get_latency(pa_simple *s, int *error) {
+ struct roarpulse_simple * ss = (struct roarpulse_simple*) s;
+ if ( !s )
+  return -1;
+
  return -1;
 }
 
 /** Flush the playback buffer. \since 0.5 */
 int pa_simple_flush(pa_simple *s, int *error) {
- return -1;
+ struct roarpulse_simple * ss = (struct roarpulse_simple*) s;
+ if ( !s )
+  return -1;
+
+ return fdatasync(ss->data_fh);
 }
 
 //ll
