a83a00538279b5fdce4511559837a55904de770d
phi <phi>
2008-08-09 22:45:18 +0000

    added simple.h and ROAR_STRDUP

--- include/libroarpulse/libroarpulse.h
+++ include/libroarpulse/libroarpulse.h
@@ -5,6 +5,12 @@
 
 #include <roaraudio.h>
 #include <pulse/pulseaudio.h>
+#include <pulse/simple.h>
+
+#include <libroarpulse/simple.h>
+
+// POSIX does not clearly specify what happens on NULL
+#define ROAR_STRDUP(x) ((x) == NULL ? NULL : strdup((x)))
 
 #endif
 
---
+++ include/libroarpulse/simple.h
@@ -0,0 +1,10 @@
+//simple.h:
+
+#ifndef _LIBROARPULSE_SIMPLE_H_
+#define _LIBROARPULSE_SIMPLE_H_
+
+#include <libroarpulse/libroarpulse.h>
+
+#endif
+
+//ll
