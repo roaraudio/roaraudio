29983cc3c637fdde5011c165b3264f1cc7dbe062
phi <phi>
2008-08-10 00:38:56 +0000

    added util.c

---
+++ libroarpulse/util.c
@@ -0,0 +1,17 @@
+//util.c:
+
+#include <libroarpulse/libroarpulse.h>
+
+/** Return the binary file name of the current process. This is not
+ * supported on all architectures, in which case NULL is returned. */
+char *pa_get_binary_name(char *s, size_t l) {
+ return NULL;
+}
+
+/** Return a pointer to the filename inside a path (which is the last
+ * component). */
+const char *pa_path_get_filename(const char *p) {
+ return NULL;
+}
+
+//ll
