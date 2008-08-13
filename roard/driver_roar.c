6e8f23809e1a2d0aa75cd6b6f1a5024621152ebc
phi <phi>
2008-08-13 00:21:17 +0000

    suppress warnings about sizeof(void*) != sizeof(int)

--- roard/driver_roar.c
+++ roard/driver_roar.c
@@ -2,6 +2,14 @@
 
 #include "roard.h"
 
+// this is to avoid warning messages on platforms
+// where sizeof(void*) == 8 and szeof(int) == 4
+#ifdef __LP64__
+#define INSTINT long int
+#else
+#define INSTINT int
+#endif
+
 int driver_roar_open(DRIVER_USERDATA_T * inst, char * device, struct roar_audio_info * info) {
 /*
  esd_format_t format = ESD_STREAM | ESD_PLAY;
@@ -18,16 +26,16 @@ int driver_roar_open(DRIVER_USERDATA_T * inst, char * device, struct roar_audio_
  return 0;
 */
 
- *(int*)inst =  roar_simple_play(info->rate, info->channels, info->bits, info->codec, device, "roard");
+ *(INSTINT*)inst =  roar_simple_play(info->rate, info->channels, info->bits, info->codec, device, "roard");
 
- if ( *(int*)inst == -1 )
+ if ( *(INSTINT*)inst == -1 )
   return -1;
 
  return 0;
 }
 
 int driver_roar_close(DRIVER_USERDATA_T   inst) {
- return roar_simple_close((int)inst);
+ return roar_simple_close((INSTINT)inst);
 }
 
 int driver_roar_pause(DRIVER_USERDATA_T   inst, int newstate) {
@@ -35,11 +43,11 @@ int driver_roar_pause(DRIVER_USERDATA_T   inst, int newstate) {
 }
 
 int driver_roar_write(DRIVER_USERDATA_T   inst, char * buf, int len) {
- return write((int)inst, buf, len);
+ return write((INSTINT)inst, buf, len);
 }
 
 int driver_roar_read(DRIVER_USERDATA_T   inst, char * buf, int len) {
- return read((int)inst, buf, len);
+ return read((INSTINT)inst, buf, len);
 }
 
 int driver_roar_flush(DRIVER_USERDATA_T   inst) {
