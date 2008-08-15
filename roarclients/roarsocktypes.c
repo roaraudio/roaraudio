c2cd6fabd5bedcea6956dc5f7874e1c4972e08c0
phi <phi>
2008-08-15 01:45:14 +0000

    wrote body

--- roarclients/roarsocktypes.c
+++ roarclients/roarsocktypes.c
@@ -3,6 +3,32 @@
 #include <roaraudio.h>
 
 int main (void) {
+ int i, fh;
+ struct {
+  char * name;
+  int (*func)(void);
+ } tests[] = {
+  {"TCPv4"           , roar_socket_new_tcp },
+  {"UDPv4"           , roar_socket_new_udp },
+  {"TCPv6"           , roar_socket_new_tcp6},
+  {"UDPv6"           , roar_socket_new_udp6},
+  {"UNIX"            , roar_socket_new_unix},
+  {"DECnet seqpacket", roar_socket_new_decnet_seqpacket},
+  {"DECnet stream"   , roar_socket_new_decnet_stream},
+  {NULL, NULL}
+ };
+
+ for (i = 0; tests[i].func; i++) {
+  printf("Type %-16s ", tests[i].name);
+  fh = tests[i].func();
+  if ( fh == -1 ) {
+   printf("not working\n");
+  } else {
+   close(fh);
+   printf("working\n");
+  }
+ }
+
  return 0;
 }
 
