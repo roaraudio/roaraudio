8bbd5c4b93537645be4fc43f083bc21354e0e683
phi <phi>
2008-08-15 23:52:17 +0000

    added IPX/SPX and display errno on error

--- roarclients/roarsocktypes.c
+++ roarclients/roarsocktypes.c
@@ -15,14 +15,16 @@ int main (void) {
   {"UNIX"            , roar_socket_new_unix},
   {"DECnet seqpacket", roar_socket_new_decnet_seqpacket},
   {"DECnet stream"   , roar_socket_new_decnet_stream},
+  {"IPX/SPX"         , roar_socket_new_ipxspx},
   {NULL, NULL}
  };
 
  for (i = 0; tests[i].func; i++) {
   printf("Type %-16s ", tests[i].name);
+  errno = 0;
   fh = tests[i].func();
   if ( fh == -1 ) {
-   printf("not working\n");
+   printf("not working: %s\n", strerror(errno));
   } else {
    close(fh);
    printf("working\n");
