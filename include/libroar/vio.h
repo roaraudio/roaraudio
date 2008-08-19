3ea31699817baea0956477a5de1e2c69815e0b9e
phi <phi>
2008-08-19 15:39:41 +0000

    added parameter inst to vio struct members

--- include/libroar/vio.h
+++ include/libroar/vio.h
@@ -7,9 +7,9 @@
 
 struct roar_vio_calls {
  void * inst;
- ssize_t (*read) (int fd, void *buf, size_t count);
- ssize_t (*write)(int fd, void *buf, size_t count);
- off_t   (*lseek)(int fildes, off_t offset, int whence);
+ ssize_t (*read) (int fd, void *buf, size_t count, inst);
+ ssize_t (*write)(int fd, void *buf, size_t count, inst);
+ off_t   (*lseek)(int fildes, off_t offset, int whence, inst);
 };
 
 int roar_vio_init_calls (struct roar_vio_calls * calls);
