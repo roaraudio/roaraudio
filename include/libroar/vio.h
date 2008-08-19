ecb8f897733244018e73cae54057f6b4ec507324
phi <phi>
2008-08-19 15:23:58 +0000

    added roar_vio_calls struct

--- include/libroar/vio.h
+++ include/libroar/vio.h
@@ -5,6 +5,12 @@
 
 #include "libroar.h"
 
+struct roar_vio_calls {
+ ssize_t (*read) (int fd, void *buf, size_t count);
+ ssize_t (*write)(int fd, void *buf, size_t count);
+ off_t   (*lseek)(int fildes, off_t offset, int whence);
+};
+
 #endif
 
 //ll
