39dd3f766cbbbd7bf6566508530b0d2e370fd7fc
phi <phi>
2008-08-27 12:13:11 +0000

    added roardsp_filter_ctl()

--- include/libroardsp/libroardsp.h
+++ include/libroardsp/libroardsp.h
@@ -13,6 +13,13 @@
 #define ROARDSP_FILTER_LOWP           2
 #define ROARDSP_FILTER_HIGHP          3
 
+// filter CTLs:
+
+#define ROARDSP_FCTL_FREQ             1
+#define ROARDSP_FCTL_TIME             2
+#define ROARDSP_FCTL_MUL              3
+#define ROARDSP_FCTL_DIV              4
+
 // types:
 
 struct roardsp_filter {
@@ -21,6 +28,7 @@ struct roardsp_filter {
  void * inst;
  int (*calc  )(struct roardsp_filter * filter, void * data, size_t samples);
  int (*uninit)(struct roardsp_filter * filter);
+ int (*ctl   )(struct roardsp_filter * filter, int cmd, void * data);
 };
 
 struct roardsp_filterchain {
@@ -40,6 +48,7 @@ char * roardsp_filter_id2str(int id);
 int    roardsp_filter_init  (struct roardsp_filter * filter, struct roar_stream * stream, int id);
 int    roardsp_filter_uninit(struct roardsp_filter * filter);
 int    roardsp_filter_calc  (struct roardsp_filter * filter, void * data, size_t len);
+int    roardsp_filter_ctl   (struct roardsp_filter * filter, int cmd, void * data);
 
 int roardsp_fchain_init  (struct roardsp_filterchain * chain);
 int roardsp_fchain_uninit(struct roardsp_filterchain * chain);
