d4d49f72ab7c6aab0a945d41c0896eac31abfb47
phi <phi>
2008-08-28 20:16:37 +0000

    added COPYING.lgplv3

--- include/roaraudio/client.h
+++ include/roaraudio/client.h
@@ -1,5 +1,30 @@
 //stream.h:
 
+/*
+ *      Copyright (C) Philipp 'ph3-der-loewe' Schafft - 2008
+ *
+ *  This file is part of RoarAudio,
+ *  a cross-platform sound system for both, home and professional use.
+ *  See README for details.
+ *
+ *  This file is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 3
+ *  as published by the Free Software Foundation.
+ *
+ *  RoarAudio is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this software; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  NOTE: Even though this file is LGPLed it (may) include GPLed files
+ *  so the license of this file is/may therefore downgraded to GPL.
+ *  See HACKING for details.
+ */
+
 #ifndef _ROARAUDIO_CLIENT_H_
 #define _ROARAUDIO_CLIENT_H_
 
