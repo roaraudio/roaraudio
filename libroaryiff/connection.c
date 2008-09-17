4446de04394fa18de6488fc050757c9255efe8c5
phi <phi>
2008-09-17 00:54:08 +0000

    check for the server to not be NULL before use strcmp()

--- libroaryiff/connection.c
+++ libroaryiff/connection.c
@@ -35,12 +35,14 @@ YConnection *YOpenConnection (const char *start_arg, const char *con_arg) {
  // there is no symbolic value for default con_arg
  // in the heder files. So we use a hard coded value from the docs
 
- if ( strcmp(server, "127.0.0.1:9433") == 0 ||
-      strcmp(server, "127.0.0.1")      == 0 ||   // \\                                         //
-      strcmp(server, "localhost")      == 0 ||   //   => Don't know if this is valid for libY  //
-      strcmp(server, "localhost:9433") == 0 ) {  // //   but we support it.                    //
+ if ( server != NULL ) {
+  if ( strcmp(server, "127.0.0.1:9433") == 0 ||
+       strcmp(server, "127.0.0.1")      == 0 ||   // \\                                         //
+       strcmp(server, "localhost")      == 0 ||   //   => Don't know if this is valid for libY  //
+       strcmp(server, "localhost:9433") == 0 ) {  // //   but we support it.                    //
 
-  server = NULL; // try default locations
+   server = NULL; // try default locations
+  }
  }
 
  if (roar_simple_connect(&con, server, name) == -1) {
