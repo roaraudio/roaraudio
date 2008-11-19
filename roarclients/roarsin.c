b0cf524650958eb87860bb3f2d496a52dd9020d5
phi <phi>
2008-11-19 21:29:07 +0000

    added support to change freq, time and form of signal

--- roarclients/roarsin.c
+++ roarclients/roarsin.c
@@ -26,18 +26,64 @@
 #include <stdio.h>      /* *printf*() */
 #include <roaraudio.h>  /* libroar */
 
-int main (void) {
+double rect (double x) {
+ x /= 2*M_PI;
+ x -= (int)x;
+
+ if ( x < 0.5 )
+  return  1;
+ else
+  return -1;
+}
+
+double saw (double x) {
+ x /= 2*M_PI;
+ x -= (int)x;
+
+ return 2*x - 1;
+}
+
+double tri (double x) {
+ x /= 2*M_PI;
+ x -= (int)x;
+
+ if ( x < 0.5 )
+  return   4* x      - 1;
+ else
+  return  -4*(x-0.5) + 1;
+}
+
+int main (int argc, char * argv[]) {
  int rate     = ROAR_RATE_DEFAULT;
- int bits     = 8;
+ int bits     = 16;
  int channels = 1; /* mono */
  int codec    = ROAR_CODEC_DEFAULT;
  float freq   = 523.2;            /* middle C */
- float step   = M_PI*2*freq/rate; /* how much time per sample we have to encode ... */
  float t      = 0; /* current time */
  float length = 5; /* 5 sec */
+ float step;       /* how much time per sample we have to encode ... */
  int fh;
  int i;
- char out[1024];
+ int16_t out[1024];
+ double (*func)(double x) = sin;
+
+ for (i = 1; i < argc; i++) {
+  if ( !strcmp(argv[i], "--freq") ) {
+   freq   = atof(argv[++i]);
+  } else if ( !strcmp(argv[i], "--time") ) {
+   length = atof(argv[++i]);
+  } else if ( !strcmp(argv[i], "--sin") ) {
+   func   = sin;
+  } else if ( !strcmp(argv[i], "--saw") ) {
+   func   = saw;
+  } else if ( !strcmp(argv[i], "--tri") ) {
+   func   = tri;
+  } else {
+   return 2;
+  }
+ }
+
+ step   = M_PI*2*freq/rate;
 
  if ( (fh = roar_simple_play(rate, channels, bits, codec, NULL, "sine gen")) == -1 ) {
   fprintf(stderr, "Error: can not open playback!\n");
@@ -46,10 +92,10 @@ int main (void) {
 
  while (t < 2*M_PI*freq*length) {
   for (i = 0; i < 1024; i++) {
-   out[i] = 127*sin(t);
+   out[i] = 32767*func(t);
    t += step;
   }
-  write(fh, out, 1024);
+  write(fh, out, 2048);
  }
 
  roar_simple_close(fh);
