bf0aa8d5e6bb4d7cf546139e5f7928c111ecc8e4
phi <phi>
2008-12-06 22:03:06 +0000

    added basic OSS driver code file

---
+++ roard/driver_oss.c
@@ -0,0 +1,34 @@
+//driver_oss.c:
+
+/*
+ *      Copyright (C) Philipp 'ph3-der-loewe' Schafft - 2008
+ *
+ *  This file is part of roard a part of RoarAudio,
+ *  a cross-platform sound system for both, home and professional use.
+ *  See README for details.
+ *
+ *  This file is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3
+ *  as published by the Free Software Foundation.
+ *
+ *  RoarAudio is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this software; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "roard.h"
+#if defined(ROAR_HAVE_OSS_BSD) || defined(ROAR_HAVE_OSS)
+
+int driver_oss_open(struct roar_vio_calls * inst, char * device, struct roar_audio_info * info);
+int driver_esd_close(DRIVER_USERDATA_T   inst);
+int driver_esd_write(struct roar_vio_calls * inst, void * buf, size_t len);
+int driver_esd_read(struct roar_vio_calls  * inst, void * buf, size_t len);
+
+#endif
+//ll
