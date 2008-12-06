e2176e1e6489c3adfd99542292df69777ccad249
phi <phi>
2008-12-06 20:07:20 +0000

    added support for vio drivers, juhu :)

--- roard/driver.c
+++ roard/driver.c
@@ -25,16 +25,16 @@
 #include "roard.h"
 
 struct roar_driver g_driver[] = {
- { "null", "null audio driver", "/dev/null", NULL, NULL, NULL, NULL, NULL, NULL},
+ { "null", "null audio driver", "/dev/null", NULL, NULL, NULL, NULL, NULL, NULL, NULL},
 #ifdef ROAR_HAVE_ESD
- { "esd", "EsounD audio driver", "localhost, remote.host.dom", driver_esd_open, driver_esd_close, driver_esd_pause, driver_esd_write, driver_esd_read, driver_esd_flush},
+ { "esd", "EsounD audio driver", "localhost, remote.host.dom", driver_esd_open, driver_esd_close, driver_esd_pause, driver_esd_write, driver_esd_read, driver_esd_flush, NULL},
 #endif
- { "roar", "RoarAudio driver", "localhost, remote.host.dom", driver_roar_open, driver_roar_close, driver_roar_pause, driver_roar_write, driver_roar_read, driver_roar_flush},
- { "raw",  "RAW PCM driver", "/some/file", driver_raw_open, driver_raw_close, driver_roar_pause, driver_raw_write, driver_raw_read, driver_raw_flush},
+ { "roar", "RoarAudio driver", "localhost, remote.host.dom", driver_roar_open, driver_roar_close, driver_roar_pause, driver_roar_write, driver_roar_read, driver_roar_flush, NULL},
+ { "raw",  "RAW PCM driver", "/some/file", driver_raw_open, driver_raw_close, driver_roar_pause, driver_raw_write, driver_raw_read, driver_raw_flush, NULL},
 #ifdef ROAR_HAVE_LIBAO
- { "ao", "libao audio driver", "DRIVER", driver_ao_open, driver_ao_close, driver_ao_pause, driver_ao_write, driver_ao_read, driver_ao_flush},
+ { "ao", "libao audio driver", "DRIVER", driver_ao_open, driver_ao_close, driver_ao_pause, driver_ao_write, driver_ao_read, driver_ao_flush, NULL},
 #endif
- {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL} // end of list
+ {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL} // end of list
                                 };
 
 void print_driverlist (void) {
@@ -56,6 +56,17 @@ int driver_open (DRIVER_USERDATA_T * inst, int * driver_id, char * driver, char 
    ROAR_DBG("driver_open(*): found driver: id = %i", i);
 
    *driver_id = i;
+
+   if ( g_driver[i].vio_init != NULL ) {
+    if ( (*inst = malloc(sizeof(struct roar_vio_calls))) == NULL )
+     return -1;
+
+    if ( (i = g_driver[i].vio_init(*inst, device, info)) == -1 ) {
+     free(*inst);
+     return -1;
+    }
+    return i;
+   }
 
    if ( g_driver[i].open )
     return g_driver[i].open(inst, device, info);
@@ -92,6 +103,9 @@ int driver_write(DRIVER_USERDATA_T   inst, int driver, char * buf, int len) {
  if ( driver == -1 )
   return -1;
 
+ if ( g_driver[driver].vio_init != NULL )
+  return roar_vio_write((struct roar_vio_calls *) inst, buf, len);
+
  if ( g_driver[driver].write )
   return g_driver[driver].write(inst, buf, len);
 
@@ -101,6 +115,9 @@ int driver_write(DRIVER_USERDATA_T   inst, int driver, char * buf, int len) {
 int driver_read (DRIVER_USERDATA_T   inst, int driver, char * buf, int len) {
  if ( driver == -1 )
   return -1;
+
+ if ( g_driver[driver].vio_init != NULL )
+  return roar_vio_read((struct roar_vio_calls *) inst, buf, len);
 
  if ( g_driver[driver].read )
   return g_driver[driver].read(inst, buf, len);
