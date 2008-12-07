f1c7d8989b96f1fb84bdbc1dc512318f74722e55
phi <phi>
2008-12-07 23:56:47 +0000

    updated roar driver to support new VIO interface

--- roard/driver_roar.c
+++ roard/driver_roar.c
@@ -32,44 +32,27 @@
 #define INSTINT int
 #endif
 
-int driver_roar_open(DRIVER_USERDATA_T * inst, char * device, struct roar_audio_info * info) {
-/*
- esd_format_t format = ESD_STREAM | ESD_PLAY;
- char name[80] = "roard";
+int driver_roar_open_vio(struct roar_vio_calls * inst, char * device, struct roar_audio_info * info, int fh) {
 
- format |= info->bits     == 16 ? ESD_BITS16 : ESD_BITS8;
- format |= info->channels ==  2 ? ESD_STEREO : ESD_MONO;
-
- *(int*)inst = esd_play_stream_fallback(format, info->rate, device, name);
-
- if ( *(int*)inst == -1 )
+ if ( fh != -1 ) { // this is a connection to a roard, no roar_simple_*() interface avalible for this case
   return -1;
+ }
 
- return 0;
-*/
-
- *(INSTINT*)inst =  roar_simple_play(info->rate, info->channels, info->bits, info->codec, device, "roard");
-
- if ( *(INSTINT*)inst == -1 )
+ if ( (fh = roar_simple_play(info->rate, info->channels, info->bits, info->codec, device, "roard")) == -1 ) {
   return -1;
+ }
+
+ roar_vio_set_fh(inst, fh);
 
  return 0;
 }
 
 int driver_roar_close(DRIVER_USERDATA_T   inst) {
- return roar_simple_close((INSTINT)inst);
+ return roar_simple_close(roar_vio_get_fh((struct roar_vio_calls *)inst));
 }
 
 int driver_roar_pause(DRIVER_USERDATA_T   inst, int newstate) {
  return -1;
-}
-
-int driver_roar_write(DRIVER_USERDATA_T   inst, char * buf, int len) {
- return write((INSTINT)inst, buf, len);
-}
-
-int driver_roar_read(DRIVER_USERDATA_T   inst, char * buf, int len) {
- return read((INSTINT)inst, buf, len);
 }
 
 int driver_roar_flush(DRIVER_USERDATA_T   inst) {
--- roard/include/driver_roar.h
+++ roard/include/driver_roar.h
@@ -25,11 +25,9 @@
 #ifndef _DRIVER_ROAR_H_
 #define _DRIVER_ROAR_H_
 
-int driver_roar_open(DRIVER_USERDATA_T * inst, char * device, struct roar_audio_info * info);
+int driver_roar_open_vio(struct roar_vio_calls * inst, char * device, struct roar_audio_info * info, int fh);
 int driver_roar_close(DRIVER_USERDATA_T   inst);
 int driver_roar_pause(DRIVER_USERDATA_T   inst, int newstate);
-int driver_roar_write(DRIVER_USERDATA_T   inst, char * buf, int len);
-int driver_roar_read(DRIVER_USERDATA_T   inst, char * buf, int len);
 int driver_roar_flush(DRIVER_USERDATA_T   inst);
 
 #endif
