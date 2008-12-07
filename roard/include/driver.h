2b4ffa11dcd04c2998098efcdd54a243aec2a99d
phi <phi>
2008-12-07 15:35:47 +0000

    use ao then oss anf finanly null driver if esd is not avalable

--- roard/include/driver.h
+++ roard/include/driver.h
@@ -32,7 +32,18 @@
 #ifdef ROAR_HAVE_ESD
 #define ROAR_DRIVER_DEFAULT "esd"
 #else
+
+#ifdef ROAR_HAVE_LIBAO
+#define ROAR_DRIVER_DEFAULT "ao"
+#else
+
+#if defined(ROAR_HAVE_OSS_BSD) || defined(ROAR_HAVE_OSS)
+#define ROAR_DRIVER_DEFAULT "oss"
+#else
+
 #define ROAR_DRIVER_DEFAULT "null"
+#endif
+#endif
 #endif
 
 #ifdef ROAR_HAVE_ESD
