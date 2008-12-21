99899ff9d18a4e92b54e91666762ce94758710cf
phi <phi>
2008-12-21 10:14:58 +0000

    print header, do the main loop as IO buffer

--- roarclients/roarmonhttp.c
+++ roarclients/roarmonhttp.c
@@ -26,55 +26,124 @@
 
 #define BUFSIZE 1024
 
+void print_header (int codec) {
+ char * mime = "application/octet-stream";
+
+ switch (codec) {
+  case ROAR_CODEC_OGG_VORBIS:
+    mime = "application/ogg";
+   break;
+ }
+
+ printf("Content-type: %s\r\n", mime);
+ printf("\r\n");
+
+ fflush(stdout);
+}
+
+int stream (int dest, int src) {
+ struct roar_buffer *ring = NULL, *cur;
+ ssize_t len;
+ size_t  todo;
+ fd_set fsi[1], fso[1];
+ struct timeval tv;
+ int alive = 1;
+ int maxfh = (dest > src ? dest : src) + 1;
+ void * data;
+
+ roar_socket_nonblock(src,  ROAR_SOCKET_NONBLOCK);
+ roar_socket_nonblock(dest, ROAR_SOCKET_NONBLOCK);
+
+ while (alive) {
+  FD_ZERO(fsi);
+  FD_ZERO(fso);
+  FD_SET(src, fsi);
+  FD_SET(dest, fso);
+
+  tv.tv_sec  = 0;
+  tv.tv_usec = 100000; // 100ms
+
+  if (select(maxfh, fsi, fso, NULL, &tv) > 0) {
+   if ( FD_ISSET(src, fsi) ) { // we can read!
+    if ( roar_buffer_new(&cur, BUFSIZE) == -1 )
+     return -1;
+
+    if ( roar_buffer_get_data(cur, &data) == -1 )
+     return -1;
+
+    len = read(src, data, BUFSIZE);
+
+    switch (len) {
+     case  0:
+     case -1:
+       roar_buffer_free(cur);
+
+       if ( ring != NULL )
+        roar_buffer_free(ring);
+
+       return -1;
+      break;
+    }
+
+    if ( ring == NULL ) {
+     ring = cur;
+    } else {
+     roar_buffer_add(ring, cur);
+    }
+   } else if ( FD_ISSET(dest, fso) && ring != NULL ) { // we can write!
+    if ( roar_buffer_get_data(ring, &data) == -1 )
+     return -1;
+
+    if ( roar_buffer_get_len(ring, &todo) == -1 )
+     return -1;
+
+    len = write(dest, data, todo);
+
+    if ( len < 1 ) {
+     if ( errno != EAGAIN ) {
+      roar_buffer_free(ring);
+      return -1;
+     }
+    }
+
+    if ( todo == len ) { // we wrote all of the pkg
+     if ( roar_buffer_next(&ring) == -1 )
+      return -1;
+    } else {
+     if ( roar_buffer_set_offset(ring, len) == -1 )
+      return -1;
+    }
+
+   }
+  }
+ }
+
+ return 0;
+}
+
 int main (int argc, char * argv[]) {
  int    rate     = 44100;
  int    bits     = 16;
  int    channels = 2;
- int    codec    = ROAR_CODEC_OGG_VORBIS;
+// int    codec    = ROAR_CODEC_OGG_VORBIS;
+ int    codec    = ROAR_CODEC_DEFAULT;
  char * server   = NULL;
- char * k;
  int    fh;
- int    i;
- int    out = -1;
- char buf[BUFSIZE];
-
- for (i = 1; i < argc; i++) {
-  k = argv[i];
-
-  if ( strcmp(k, "--server") == 0 ) {
-   server = argv[++i];
-  } else if ( strcmp(k, "--rate") == 0 ) {
-   rate = atoi(argv[++i]);
-  } else if ( strcmp(k, "--bits") == 0 ) {
-   bits = atoi(argv[++i]);
-  } else if ( strcmp(k, "--channels") == 0 ) {
-   channels = atoi(argv[++i]);
-  } else if ( strcmp(k, "--codec") == 0 ) {
-   codec = roar_str2codec(argv[++i]);
-  } else if ( strcmp(k, "--help") == 0 ) {
-   return 0;
-  } else if ( out == -1 ) {
-   if ( (out = open(k, O_CREAT|O_TRUNC|O_WRONLY, 0644)) == -1 ) {
-    fprintf(stderr, "Error: can not open file: %s: %s\n", k, strerror(errno));
-    return 1;
-   }
-  } else {
-   fprintf(stderr, "Error: unknown argument: %s\n", k);
-   return 1;
-  }
- }
-
- if ( out == -1 )
-  out = ROAR_STDOUT;
 
  if ( (fh = roar_simple_monitor(rate, channels, bits, codec, server, "roarmon")) == -1 ) {
   fprintf(stderr, "Error: can not start monitoring\n");
   return 1;
  }
 
+ print_header(codec);
+
+/*
  while((i = read(fh, buf, BUFSIZE)))
   if (write(out, buf, i) != i)
    break;
+*/
+
+ stream(ROAR_STDOUT, fh);
 
  roar_simple_close(fh);
 
