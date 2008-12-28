1f006b15c649dc6ca419ac061bb0e63a857edad7
phi <phi>
2008-12-28 17:25:54 +0000

    no stat() and struct stat in win32...

--- libroar/simple.c
+++ libroar/simple.c
@@ -146,12 +146,16 @@ int roar_simple_new_stream_obj (struct roar_connection * con, struct roar_stream
   port = ROAR_NET2HOST16(socket_addr.sin_port);
   ROAR_DBG("roar_simple_new_stream_obj(*): port=%i", port);
  } else if ( type == ROAR_SOCKET_TYPE_UNIX ) {
+#ifndef ROAR_TARGET_WIN32
   chmod(file, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
 
   grp = getgrnam(ROAR_DEFAULT_SOCKGRP);
 
   if ( grp )
    chown(file, -1, grp->gr_gid);
+#else
+  ROAR_ERR("roar_simple_new_stream_obj(*): There is no UNIX Domain Socket support in win32, download a real OS.");
+#endif
  } else if ( type == ROAR_SOCKET_TYPE_DECNET ) {
   len = sizeof(struct sockaddr_in);
   setsockopt(listen, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int));
