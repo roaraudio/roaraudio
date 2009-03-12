d6774700d831e6dcc00826f054fb4ada451ff095
phi <phi>
2009-03-12 01:02:37 +0000

    added call to fh -> vio for sockets, needed for win32 in future

--- include/libroar/vio.h
+++ include/libroar/vio.h
@@ -98,6 +98,7 @@ int     roar_vio_close   (struct roar_vio_calls * vio);
 // converters:
 int     roar_vio_open_file     (struct roar_vio_calls * calls, char * filename, int flags, mode_t mode);
 int     roar_vio_open_fh       (struct roar_vio_calls * calls, int fh);
+int     roar_vio_open_fh_socket(struct roar_vio_calls * calls, int fh);
 
 int     roar_vio_simple_stream (struct roar_vio_calls * calls, int rate, int channels, int bits, int codec,
                                                                char * server, int dir, char * name);
--- libroar/vio.c
+++ libroar/vio.c
@@ -175,6 +175,13 @@ int     roar_vio_open_fh       (struct roar_vio_calls * calls, int fh) {
  return roar_vio_set_fh(calls, fh);
 }
 
+int     roar_vio_open_fh_socket(struct roar_vio_calls * calls, int fh) {
+ if ( calls == NULL )
+  return -1;
+
+ return roar_vio_open_fh(calls, fh);
+}
+
 int     roar_vio_simple_stream (struct roar_vio_calls * calls, int rate, int channels, int bits, int codec,
                                                                char * server, int dir, char * name) {
  int fh;
@@ -185,7 +192,7 @@ int     roar_vio_simple_stream (struct roar_vio_calls * calls, int rate, int cha
  if ( (fh = roar_simple_stream(rate, channels, bits, codec, server, dir, name)) == -1 )
   return -1;
 
- return roar_vio_open_fh(calls, fh);
+ return roar_vio_open_fh_socket(calls, fh);
 }
 
 
