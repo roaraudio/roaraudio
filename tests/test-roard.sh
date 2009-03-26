82261453b86d8b14e3e4b4db17a2a131ad72fd9b
phi <phi>
2009-03-26 13:26:01 +0000

    addes test-roard.sh

---
+++ tests/test-roard.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TEST_NAME='roard test'
+TEST_DESC='Do tests on RoarAudio'\''s Sound Daemon'
+
+. ./common.sh
+
+c=roard
+FORMAT="%-42s : "
+
+printf "$FORMAT" 'Testing for general runnability'
+if $LD_LIBRARY_PATH$c --no-listen --terminate --client-fh 0 < /dev/null > /dev/null 2> /dev/null
+then
+ _ok
+ echo ok
+else
+ _failed
+ echo failed
+fi
+
+disp_sum
+ret
+
+#ll
