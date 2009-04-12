6f45e863b29a0beb85537a2d7dbab36d6dc55640
phi <phi>
2009-04-12 23:37:23 +0000

    wrote a basic sio_setpar()

--- libroarsndio/para.c
+++ libroarsndio/para.c
@@ -53,7 +53,60 @@ void   sio_initpar(struct sio_par * par) {
 }
 
 int    sio_setpar (struct sio_hdl * hdl, struct sio_par * par) {
- return 0;
+ if ( hdl == NULL )
+  return 0;
+
+ if ( par->bits == 0 || par->bits > ROAR_BITS_MAX )
+  return 0;
+
+ if ( par->bps == 0 )
+  par->bps = SIO_BPS(par->bits);
+
+ if ( par->bps > ROAR_BITS_MAX/8 )
+  return 0;
+
+ if ( SIO_BPS(par->bits) > par->bps )
+  return 0;
+
+ hdl->info.bits = par->bps * 8;
+
+ switch ((par->sig << 4) | par->le) {
+  case 0x00:
+    hdl->info.codec = ROAR_CODEC_PCM_U_BE;
+   break;
+  case 0x01:
+    hdl->info.codec = ROAR_CODEC_PCM_U_LE;
+   break;
+  case 0x10:
+    hdl->info.codec = ROAR_CODEC_PCM_S_BE;
+   break;
+  case 0x11:
+    hdl->info.codec = ROAR_CODEC_PCM_S_LE;
+   break;
+  default:
+    return 0;
+ }
+
+ if ( par->msb == 0 )
+  return 0;
+
+ if ( par->rchan != 0 ) /* not supported yet */
+  return 0;
+
+ if ( par->pchan == 0 || par->pchan > ROAR_MAX_CHANNELS )
+  return 0;
+
+ hdl->info.channels = par->pchan;
+
+ if ( par->rate == 0 )
+  return 0;
+
+ hdl->info.rate = par->rate;
+
+ if ( par->xrun != SIO_IGNORE )
+  return 0;
+
+ return 1;
 }
 
 int    sio_getpar (struct sio_hdl * hdl, struct sio_par * par) {
