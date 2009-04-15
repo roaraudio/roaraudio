d7f5a2d69d2c5e5b643092dd29b40342bef33429
phi <phi>
2009-04-15 22:01:29 +0000

    wrote some basic code

--- libroardsp/filter_swap.c
+++ libroardsp/filter_swap.c
@@ -25,15 +25,46 @@
 #include "libroardsp.h"
 
 int roardsp_swap_init   (struct roardsp_filter * filter, struct roar_stream * stream, int id) {
- return -1;
+ struct roardsp_swap * inst = malloc(sizeof(struct roardsp_swap));
+
+ ROAR_WARN("roardsp_swap_init(*): inst=%p", inst);
+
+ if ( inst == NULL ) {
+  ROAR_ERR("roardsp_swap_init(*): Can not alloc memory for filter swap: %s", strerror(errno));
+  filter->inst = NULL;
+  return -1;
+ }
+
+ filter->inst = inst;
+
+ return roardsp_swap_reset(filter, ROARDSP_RESET_FULL);
 }
 
 int roardsp_swap_uninit (struct roardsp_filter * filter) {
- return -1;
+ if ( filter == NULL )
+  return -1;
+
+ if ( filter->inst == NULL )
+  return -1;
+
+ free(filter->inst);
+
+ return 0;
 }
 
 int roardsp_swap_calc162(struct roardsp_filter * filter, void * data, size_t samples) {
- return -1;
+ int16_t * d = data;
+ register int16_t s;
+ int i;
+
+ for (i = 0; i < samples; i += 2) {
+  ROAR_WARN("roardsp_swap_calc162(*): d[i]=%i, d[i+1]=%i", d[i], d[i+1]);
+  s      = d[i];
+  d[i]   = d[i+1];
+  d[i+1] = s;
+ }
+
+ return 0;
 }
 
 int roardsp_swap_ctl    (struct roardsp_filter * filter, int cmd, void * data) {
@@ -41,6 +72,34 @@ int roardsp_swap_ctl    (struct roardsp_filter * filter, int cmd, void * data) {
 }
 
 int roardsp_swap_reset  (struct roardsp_filter * filter, int what) {
+ struct roardsp_swap * inst = NULL;
+ int i;
+
+ if ( filter == NULL )
+  return -1;
+
+ if ( (inst = filter->inst) == NULL )
+  return -1;
+
+ switch (what) {
+  case ROARDSP_RESET_NONE:
+  case ROARDSP_RESET_STATE:
+    return  0;
+   break;
+  case ROARDSP_RESET_FULL:
+    memset(inst, 0, sizeof(struct roardsp_swap));
+
+    for (i = 0; i < ROAR_MAX_CHANNELS; i++)
+     inst->map[i] = i;
+
+    inst->map[0] = 1;
+    inst->map[1] = 0;
+    return  0;
+   break;
+  default:
+    return -1;
+ }
+
  return -1;
 }
 
