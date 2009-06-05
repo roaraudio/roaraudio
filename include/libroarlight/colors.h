fe35f88aa07c3858e903ddc06a74e4f7e1677200
phi <phi>
2009-06-05 15:57:21 +0000

    added some defines for color types

--- include/libroarlight/colors.h
+++ include/libroarlight/colors.h
@@ -37,6 +37,37 @@
 
 #include "libroarlight.h"
 
+#define ROAR_COLOR_TYPE_NONE           0x00000000
+#define ROAR_COLOR_TYPE_GRAY           0x00000001 /* ???         */
+#define ROAR_COLOR_TYPE_ALPHA          0x00000002 /* Alpha       */
+#define ROAR_COLOR_TYPE_DOUBLE         0x00000004 /* use 16 bit per channel */
+#define ROAR_COLOR_TYPE_A              ROAR_COLOR_TYPE_ALPHA
+#define ROAR_COLOR_TYPE_K              0x00000010 /* black (Key) */
+#define ROAR_COLOR_TYPE_R              0x00000020 /* red         */
+#define ROAR_COLOR_TYPE_G              0x00000040 /* green       */
+#define ROAR_COLOR_TYPE_B              0x00000080 /* blue        */
+#define ROAR_COLOR_TYPE_H              0x00000100
+#define ROAR_COLOR_TYPE_S              0x00000200
+#define ROAR_COLOR_TYPE_V              0x00000400
+#define ROAR_COLOR_TYPE_B              0x00000800
+#define ROAR_COLOR_TYPE_L              0x00001000
+#define ROAR_COLOR_TYPE_I              0x00002000
+#define ROAR_COLOR_TYPE_C              0x00004000 /* Cyan          */
+#define ROAR_COLOR_TYPE_M              0x00008000 /* Magenta       */
+#define ROAR_COLOR_TYPE_Y              0x00010000 /* Yellow or ??? */
+#define ROAR_COLOR_TYPE_U              0x00020000
+//#define ROAR_COLOR_TYPE_              0x0100
+
+#define ROAR_COLORSYSTEM_GRAY          (ROAR_COLOR_TYPE_K)
+#define ROAR_COLORSYSTEM_RGB           (ROAR_COLOR_TYPE_R|ROAR_COLOR_TYPE_G|ROAR_COLOR_TYPE_B)
+#define ROAR_COLORSYSTEM_RGBA          (ROAR_COLOR_TYPE_R|ROAR_COLOR_TYPE_G|ROAR_COLOR_TYPE_B|ROAR_COLOR_TYPE_ALPHA)
+#define ROAR_COLORSYSTEM_YUV           (ROAR_COLOR_TYPE_Y|ROAR_COLOR_TYPE_U|ROAR_COLOR_TYPE_V)
+#define ROAR_COLORSYSTEM_HSV           (ROAR_COLOR_TYPE_H|ROAR_COLOR_TYPE_S|ROAR_COLOR_TYPE_V)
+#define ROAR_COLORSYSTEM_HSL           (ROAR_COLOR_TYPE_H|ROAR_COLOR_TYPE_S|ROAR_COLOR_TYPE_L)
+#define ROAR_COLORSYSTEM_HSB           (ROAR_COLOR_TYPE_H|ROAR_COLOR_TYPE_S|ROAR_COLOR_TYPE_B)
+#define ROAR_COLORSYSTEM_HSI           (ROAR_COLOR_TYPE_H|ROAR_COLOR_TYPE_S|ROAR_COLOR_TYPE_I)
+#define ROAR_COLORSYSTEM_CMYK          (ROAR_COLOR_TYPE_C|ROAR_COLOR_TYPE_M|ROAR_COLOR_TYPE_Y|ROAR_COLOR_TYPE_K)
+
 #endif
 
 //ll
