b3390cbdf58224928166ae4a88a339f6d825bd75
phi <phi>
2009-08-05 02:31:11 +0000

    added transcode_speex.c

---
+++ libroardsp/transcode_speex.c
@@ -0,0 +1,34 @@
+//transcode_speex.c:
+
+/*
+ *      Copyright (C) Philipp 'ph3-der-loewe' Schafft - 2009
+ *
+ *  This file is part of libroardsp a part of RoarAudio,
+ *  a cross-platform sound system for both, home and professional use.
+ *  See README for details.
+ *
+ *  This file is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3
+ *  as published by the Free Software Foundation.
+ *
+ *  libroardsp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this software; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "libroardsp.h"
+
+#ifdef ROAR_HAVE_LIBSPEEX
+
+#define _16BIT (16/8)
+#define _SIZE_LEN 2
+
+#endif
+
+//ll
