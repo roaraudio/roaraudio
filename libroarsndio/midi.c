d6d96034f76228e43dba2adb8f47951caa5abaf7
phi <phi>
2009-08-14 19:12:43 +0000

    just aliases

--- libroarsndio/midi.c
+++ libroarsndio/midi.c
@@ -33,13 +33,36 @@
 #define ROAR_USE_OWN_SNDIO_HDL
 #include "libroarsndio.h"
 
-struct mio_hdl * mio_open   (const char * name, unsigned mode, int nbio_flag);
-void             mio_close  (struct mio_hdl * hdl);
-size_t           mio_write  (struct mio_hdl * hdl, const void * addr, size_t nbytes);
-size_t           mio_read   (struct mio_hdl * hdl, void * addr, size_t nbytes);
-int              mio_nfds   (struct mio_hdl * hdl);
-int              mio_pollfd (struct mio_hdl * hdl, struct pollfd * pfd, int events);
-int              mio_revents(struct mio_hdl * hdl, struct pollfd * pfd);
-int              mio_eof    (struct mio_hdl * hdl);
+struct mio_hdl * mio_open   (const char * name, unsigned mode, int nbio_flag) {
+ return sio_open(name, mode, nbio_flag);
+}
+
+void             mio_close  (struct mio_hdl * hdl) {
+ return sio_close(hdl);
+}
+
+size_t           mio_write  (struct mio_hdl * hdl, const void * addr, size_t nbytes) {
+ return sio_write(hdl, addr, nbytes);
+}
+
+size_t           mio_read   (struct mio_hdl * hdl, void * addr, size_t nbytes) {
+ return sio_read(hdl, addr, nbytes);
+}
+
+int              mio_nfds   (struct mio_hdl * hdl) {
+ return mio_nfds(hdl);
+}
+
+int              mio_pollfd (struct mio_hdl * hdl, struct pollfd * pfd, int events) {
+ return sio_pollfd(hdl, pfd, events);
+}
+
+int              mio_revents(struct mio_hdl * hdl, struct pollfd * pfd) {
+ return sio_revents(hdl, pfd);
+}
+
+int              mio_eof    (struct mio_hdl * hdl) {
+ return sio_eof(hdl);
+}
 
 //ll
