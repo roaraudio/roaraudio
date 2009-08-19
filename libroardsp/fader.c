fbbe9d0ac407cf4451904f647df4e492cc7ef4df
phi <phi>
2009-08-19 18:54:21 +0000

    some basic code

--- libroardsp/fader.c
+++ libroardsp/fader.c
@@ -24,5 +24,63 @@
 
 #include "libroardsp.h"
 
+#define _CHECK_BASIC()   if ( state == NULL ) return -1
+#define _CHECK_CALCPCM() _CHECK_BASIC(); if ( frames == 0 ) return 0; if ( data == NULL ) return -1;
+
+int roar_fader_init         (struct roar_fader_state * state, float * poly, int coeff) {
+ _CHECK_BASIC();
+
+ if ( poly == NULL )
+  return -1;
+
+ if ( coeff < 2 )
+  return -1;
+
+ if ( coeff > ROAR_FADER_MAX_COEFF )
+  return -1;
+
+ memset(state, 0, sizeof(struct roar_fader_state));
+
+ state->rate  = -1;
+ state->coeff = coeff;
+
+ memcpy(&(state->poly), poly, sizeof(float)*coeff);
+
+ return 0;
+}
+
+int roar_fader_set_rate     (struct roar_fader_state * state, int rate) {
+ _CHECK_BASIC();
+
+ state->rate = rate;
+
+ return 0;
+}
+
+int roar_fader_set_startstop(struct roar_fader_state * state, ssize_t start, ssize_t stop) {
+ _CHECK_BASIC();
+
+ if ( start >= 0 )
+  state->start = start;
+
+ if ( stop  >= 0 )
+  state->stop  = stop;
+
+ return 0;
+}
+
+int roar_fader_calcpcm_i16n(struct roar_fader_state * state, int16_t * data, size_t frames, int channels) {
+ _CHECK_CALCPCM();
+
+ switch (channels) {
+  case 1: return roar_fader_calcpcm_i161(state, data, frames);
+ }
+
+ return -1;
+}
+
+int roar_fader_calcpcm_i161(struct roar_fader_state * state, int16_t * data, size_t frames) {
+ return -1;
+}
 
 //ll
