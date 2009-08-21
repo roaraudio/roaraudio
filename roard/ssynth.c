389465c8b2f77cf49d72dddca56a4fcd84a5736f
phi <phi>
2009-08-21 02:59:18 +0000

    added code to init a stream

--- roard/ssynth.c
+++ roard/ssynth.c
@@ -30,18 +30,58 @@ int ssynth_init_config(void) {
  return 0;
 }
 
+#define _err() streams_delete(sid); return -1
 int ssynth_init (void) {
+ struct roar_stream_server * ss;
+ struct roar_stream        *  s;
+ int sid;
+
  if ( !ssynth_conf.enable )
   return 0;
 
- return -1;
+ memset(&g_ssynth, 0, sizeof(g_ssynth));
+ g_ssynth.stream = -1;
+
+ if ( (sid = streams_new()) == -1 )
+  return -1;
+
+ if ( streams_set_client(sid, g_self_client) == -1 ) {
+  _err();
+ }
+
+ if ( streams_set_dir(sid, ROAR_DIR_BRIDGE, 1) == -1 ) {
+  _err();
+ }
+
+ if ( streams_set_flag(sid, ROAR_FLAG_PRIMARY) == -1 ) {
+  _err();
+ }
+
+ if ( streams_set_name(sid, "Simple Synthesizer") == -1 ) {
+  _err();
+ }
+
+ if ( streams_get(sid, &ss) == -1 ) {
+  _err();
+ }
+
+ s = ROAR_STREAM(ss);
+
+ memcpy(&(s->info), g_sa, sizeof(struct roar_audio_info));
+
+ s->info.channels = 1;
+ s->info.codec    = ROAR_CODEC_DEFAULT;
+
+ g_ssynth.stream = sid;
+
+ return 0;
 }
 
 int ssynth_free (void) {
  if ( !ssynth_conf.enable )
   return 0;
 
- return -1;
+ return streams_delete(g_ssynth.stream);
 }
 
 //ll
