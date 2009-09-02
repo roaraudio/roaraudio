941f43fd3e542fecad0adb65cad506b987d6e2c5
phi <phi>
2009-09-02 10:56:32 +0000

    only test midi stuff if we have it

--- roarclients/roartypes.c
+++ roarclients/roartypes.c
@@ -42,7 +42,9 @@ int main (void) {
   { "roar_stack",          sizeof(struct roar_stack)          },
   { "roar_vio_defaults",   sizeof(struct roar_vio_defaults)   },
   { "roar_vio_dstr_chain", sizeof(struct roar_vio_dstr_chain) },
+#ifndef ROAR_WITHOUT_DCOMP_MIDI
   { "midi_message",        sizeof(struct midi_message)        },
+#endif
   { "roar_note_octave",    sizeof(struct roar_note_octave)    },
   { NULL, 0 }
  }, * c = types - 1;
