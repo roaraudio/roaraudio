e8332678d8da1303d741be57c8587233dac15717
phi <phi>
2009-09-13 00:33:49 +0000

    some basic callbacks for RAUM

--- roard/codecfilter_raum.c
+++ roard/codecfilter_raum.c
@@ -25,8 +25,55 @@
 #include "roard.h"
 
 #ifdef ROAR_HAVE_LIBRAUM
+#include <raum.h>
+
+int cf_raum_pcb_open  (struct cont_fw_parent_inst * self,
+                       int                          codec,
+                       struct roar_stream_server  * stream,
+                       struct roar_codecfilter    * filter) {
+ struct roar_stream * s = ROAR_STREAM(stream);
+ RAUMState * state;
+ int flags = 0;
+
+ if ( stream == NULL )
+  return -1;
+
+ switch (s->dir) {
+  case ROAR_DIR_COMPLEX_IN:
+  case ROAR_DIR_PLAY:
+    flags = O_RDONLY;
+   break;
+  case ROAR_DIR_COMPLEX_OUT:
+  case ROAR_DIR_MONITOR:
+  case ROAR_DIR_OUTPUT:
+    flags = O_WRONLY;
+   break;
+  default:
+    return -1;
+ }
+
+ state = RAUMOpenVIO(&(stream->vio), flags);
+
+ if ( state == NULL )
+  return -1;
+
+ cont_fw_set_uinst(self, state);
+
+ return 0;
+}
+
+int cf_raum_pcb_close (struct cont_fw_parent_inst * self) {
+ void * state;
+
+ if ( cont_fw_get_uinst(self, &state) == -1 )
+  return -1;
+
+ return RAUMClose(state);
+}
 
 CONT_FW_SETUP_TYPE(cf_raum_setup) {
+ self->pcb.open  = cf_raum_pcb_open;
+ self->pcb.close = cf_raum_pcb_close;
  return 0;
 }
 
