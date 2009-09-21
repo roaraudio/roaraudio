9e339f26775b5c70f8d437a2cbbc8f2a98433e64
phi <phi>
2009-09-21 00:18:04 +0000

    new prototypes roar_conv_chans_2to{3,4,5,6}{8,16}

--- include/libroardsp/convert.h
+++ include/libroardsp/convert.h
@@ -60,10 +60,20 @@ int roar_conv_chans_1ton8  (void * out, void * in, int samples, int to);   // ye
 int roar_conv_chans_1ton16 (void * out, void * in, int samples, int to);   // yes          yes
 int roar_conv_chans_1to28  (void * out, void * in, int samples);           // yes          yes
 int roar_conv_chans_1to216 (void * out, void * in, int samples);           // yes          yes
+
 int roar_conv_chans_nto18  (void * out, void * in, int samples, int from); // yes          ?
 int roar_conv_chans_nto116 (void * out, void * in, int samples, int from); // yes          ?
+
 int roar_conv_chans_2to18  (void * out, void * in, int samples);           // yes          yes
 int roar_conv_chans_2to116 (void * out, void * in, int samples);           // yes          yes
+int roar_conv_chans_2to38  (void * out, void * in, int samples);           // ?            ?
+int roar_conv_chans_2to316 (void * out, void * in, int samples);           // yes          yes
+int roar_conv_chans_2to48  (void * out, void * in, int samples);           // ?            ?
+int roar_conv_chans_2to416 (void * out, void * in, int samples);           // ?            ?
+int roar_conv_chans_2to58  (void * out, void * in, int samples);           // ?            ?
+int roar_conv_chans_2to516 (void * out, void * in, int samples);           // ?            ?
+int roar_conv_chans_2to68  (void * out, void * in, int samples);           // ?            ?
+int roar_conv_chans_2to616 (void * out, void * in, int samples);           // ?            ?
 
 int roar_conv_rate       (void * out, void * in, int samples, int from, int to, int bits, int channels);
 int roar_conv_rate_8     (void * out, void * in, int samples, int from, int to, int channels);
