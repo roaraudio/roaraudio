fab0d04ca45cabc71d7a06adf459a29d10456ee7
phi <phi>
2009-09-26 11:03:27 +0000

    set byte order things correctly on win32

--- include/roaraudio/win32hacks.h
+++ include/roaraudio/win32hacks.h
@@ -55,6 +55,19 @@
 #define ROAR_NETWORK_WRITE(x,y,z) write((x), (y), (z))
 #endif
 
+#ifndef LITTLE_ENDIAN
+#define LITTLE_ENDIAN 1234
+#endif
+#ifndef BIG_ENDIAN
+#define BIG_ENDIAN    4321
+#endif
+#ifndef PDP_ENDIAN
+#define PDP_ENDIAN    3412
+#endif
+
+#ifndef BYTE_ORDER
+#define BYTE_ORDER LITTLE_ENDIAN
+#endif
 
 #endif
 
