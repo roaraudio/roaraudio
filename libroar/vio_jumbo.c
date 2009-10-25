b5767c4e333e029a3f699d8d1c63e7e365b2d302
phi <phi>
2009-10-25 20:50:54 +0000

    wrote the jumbo layer

--- libroar/vio_jumbo.c
+++ libroar/vio_jumbo.c
@@ -34,4 +34,114 @@
 
 #include "libroar.h"
 
+int     roar_vio_open_jumbo    (struct roar_vio_calls * calls, struct roar_vio_calls * vio, size_t buffersize) {
+ struct roar_vio_jumbo * self;
+
+ if ( (self = roar_mm_malloc(sizeof(struct roar_vio_jumbo))) == NULL ) {
+  return -1;
+ }
+
+ memset(self, 0, sizeof(struct roar_vio_jumbo));
+
+ self->backend = vio;
+
+ if ( roar_buffer_new(&(self->buffer), buffersize) == -1 ) {
+  roar_mm_free(self);
+  return -1;
+ }
+
+ memset(calls, 0, sizeof(struct roar_vio_calls));
+ calls->inst     = self;
+ calls->close    = roar_vio_jumbo_close;
+ calls->read     = roar_vio_jumbo_read;
+ calls->write    = roar_vio_jumbo_write;
+ calls->lseek    = roar_vio_jumbo_lseek;
+ calls->nonblock = roar_vio_jumbo_nonblock;
+ calls->sync     = roar_vio_jumbo_sync;
+ calls->ctl      = roar_vio_jumbo_ctl;
+
+ return 0;
+}
+
+int     roar_vio_jumbo_close   (struct roar_vio_calls * vio) {
+ struct roar_vio_jumbo * self = vio->inst;
+
+ if ( roar_vio_jumbo_sync(vio) == -1 )
+  return -1;
+
+ roar_buffer_free(self->buffer);
+ roar_mm_free(self);
+
+ return 0;
+}
+
+ssize_t roar_vio_jumbo_read    (struct roar_vio_calls * vio, void *buf, size_t count) {
+ struct roar_vio_jumbo * self = vio->inst;
+
+ return roar_vio_read(self->backend, buf, count);
+}
+
+ssize_t roar_vio_jumbo_write   (struct roar_vio_calls * vio, void *buf, size_t count) {
+ struct roar_vio_jumbo * self = vio->inst;
+ size_t   buflen;
+ void   * data;
+
+ if ( roar_buffer_get_len(self->buffer, &buflen) == -1 )
+  return -1;
+
+ if ( roar_buffer_get_data(self->buffer, &data) == -1 )
+  return -1;
+
+ if ( (self->pos + count) > buflen ) {
+  if ( roar_vio_jumbo_sync(vio) == -1 )
+   return -1;
+
+  memcpy(data, buf, count);
+  self->pos = count;
+ } else {
+  memcpy(data + self->pos, buf, count);
+  self->pos += count;
+ }
+
+ return 0;
+}
+
+off_t   roar_vio_jumbo_lseek   (struct roar_vio_calls * vio, off_t offset, int whence) {
+ struct roar_vio_jumbo * self = vio->inst;
+
+ if ( roar_vio_jumbo_sync(vio) == -1 )
+  return (off_t) -1;
+
+ return roar_vio_lseek(self->backend, offset, whence);
+}
+
+int     roar_vio_jumbo_nonblock(struct roar_vio_calls * vio, int state) {
+ struct roar_vio_jumbo * self = vio->inst;
+
+ return roar_vio_nonblock(self->backend, state);
+}
+
+int     roar_vio_jumbo_sync    (struct roar_vio_calls * vio) {
+ struct roar_vio_jumbo * self = vio->inst;
+ void                  * data;
+
+ if ( self->pos == 0 )
+  return 0;
+
+ if ( roar_buffer_get_data(self->buffer, &data) == -1 )
+  return -1;
+
+ // TODO: do this a bit more intelergent (RE?)
+ if ( roar_vio_write(self->backend, data, self->pos) != self->pos )
+  return -1;
+
+ self->pos = 0;
+
+ return 0;
+}
+
+int     roar_vio_jumbo_ctl     (struct roar_vio_calls * vio, int cmd, void * data) {
+ return -1;
+}
+
 //ll
