8387ce7b841cc92020f809a1fcb720e1f89f3a34
phi <phi>
2010-01-15 18:40:44 +0000

    removed useless keyword extern

--- libroaross/libroaross.c
+++ libroaross/libroaross.c
@@ -749,7 +749,7 @@ ssize_t read(int fd, void *buf, size_t count) {
  return _os.read(fd, buf, count);
 }
 
-extern int ioctl (int __fd, unsigned long int __request, ...) {
+int ioctl (int __fd, unsigned long int __request, ...) {
  struct pointer * pointer;
  struct handle  * handle;
  va_list args;
