67c2a0c4b5a226431d047dbfd87c1c74a03d8384
phi <phi>
2010-01-29 16:57:48 +0000

    update correct counter

--- libroaross/libroaross.c
+++ libroaross/libroaross.c
@@ -810,7 +810,7 @@ ssize_t read(int fd, void *buf, size_t count) {
    }
    ret = roar_vio_read(&(pointer->handle->stream_vio), buf, count);
    if ( ret > 0 )
-    pointer->handle->writec += ret;
+    pointer->handle->readc += ret;
    return ret;
   } else {
    errno = EINVAL;
