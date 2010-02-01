be0fd8cd9f3382b927c53a53999fe63e8c62d286
phi <phi>
2010-02-01 10:08:58 +0000

    start with non-IO ops

--- libroar/vio_rtp.c
+++ libroar/vio_rtp.c
@@ -35,14 +35,81 @@
 #include "libroar.h"
 
 int roar_vio_open_rtp        (struct roar_vio_calls * calls, struct roar_vio_calls * dst,
-                              char * dstr, struct roar_vio_defaults * odef);
+                              char * dstr, struct roar_vio_defaults * odef) {
+ struct roar_rtp_inst * self = NULL;
+
+ if ( calls == NULL || dst == NULL )
+  return -1;
+
+ if ( (self = roar_mm_malloc(sizeof(struct roar_rtp_inst))) == NULL )
+  return -1;
+
+ memset(self, 0, sizeof(struct roar_rtp_inst));
+
+ self->vio                 = dst;
+
+ self->header.version      = 2;
+ self->header.payload_type = ROAR_RTP_PT_UNKNOWN;
+
+ memset(calls, 0, sizeof(struct roar_vio_calls));
+
+ calls->inst               = self;
+// calls->read               = roar_vio_rtp_read;
+// calls->write              = roar_vio_rtp_write;
+// calls->lseek              = roar_vio_rtp_lseek;
+ calls->nonblock           = roar_vio_rtp_nonblock;
+ calls->sync               = roar_vio_rtp_sync;
+ calls->ctl                = roar_vio_rtp_ctl;
+ calls->close              = roar_vio_rtp_close;
+
+ return 0;
+}
 
 ssize_t roar_vio_rtp_read    (struct roar_vio_calls * vio, void *buf, size_t count);
 ssize_t roar_vio_rtp_write   (struct roar_vio_calls * vio, void *buf, size_t count);
 off_t   roar_vio_rtp_lseek   (struct roar_vio_calls * vio, off_t offset, int whence);
-int     roar_vio_rtp_nonblock(struct roar_vio_calls * vio, int state);
-int     roar_vio_rtp_sync    (struct roar_vio_calls * vio);
-int     roar_vio_rtp_ctl     (struct roar_vio_calls * vio, int cmd, void * data);
-int     roar_vio_rtp_close   (struct roar_vio_calls * vio);
+
+int     roar_vio_rtp_nonblock(struct roar_vio_calls * vio, int state) {
+ struct roar_rtp_inst * self = vio->inst;
+
+ return roar_vio_nonblock(self->vio, state);
+}
+
+int     roar_vio_rtp_sync    (struct roar_vio_calls * vio) {
+ struct roar_rtp_inst * self = vio->inst;
+
+ return roar_vio_sync(self->vio);
+}
+
+int     roar_vio_rtp_ctl     (struct roar_vio_calls * vio, int cmd, void * data) {
+ struct roar_rtp_inst * self = vio->inst;
+
+ if (vio == NULL || cmd == -1)
+  return -1;
+
+ switch (cmd) {
+  case ROAR_VIO_CTL_GET_NEXT:
+    *(struct roar_vio_calls **)data = self->vio;
+    return 0;
+   break;
+  case ROAR_VIO_CTL_SET_NEXT:
+    self->vio = *(struct roar_vio_calls **)data;
+    return 0;
+   break;
+ }
+
+ return roar_vio_ctl(self->vio, cmd, data);
+}
+
+int     roar_vio_rtp_close   (struct roar_vio_calls * vio) {
+ struct roar_rtp_inst * self = vio->inst;
+ int ret;
+
+ ret = roar_vio_close(self->vio);
+
+ roar_mm_free(self);
+
+ return ret;
+}
 
 //ll
