da8f9bb820b5611877ff5b504c51df1049088759
phi <phi>
2010-02-11 16:55:21 +0000

    start with those strange formating functions

--- libroarpulse/sample.c
+++ libroarpulse/sample.c
@@ -42,10 +42,35 @@
 size_t pa_bytes_per_second(const pa_sample_spec *spec);
 
 /** Return the size of a frame with the specific sample type */
-size_t pa_frame_size(const pa_sample_spec *spec);
+size_t pa_frame_size(const pa_sample_spec *spec) {
+ if ( spec == NULL )
+  return 0;
+
+ return pa_sample_size(spec) * spec->channels;
+}
 
 /** Return the size of a sample with the specific sample type */
-size_t pa_sample_size(const pa_sample_spec *spec);
+size_t pa_sample_size(const pa_sample_spec *spec) {
+ if ( spec == NULL )
+  return -1;
+
+ switch (spec->format) {
+  case PA_SAMPLE_ALAW:
+  case PA_SAMPLE_ULAW:
+  case PA_SAMPLE_U8:
+    return 1;
+   break;
+  case PA_SAMPLE_S16LE:
+  case PA_SAMPLE_S16BE:
+    return 2;
+   break;
+  default:
+    return 0;
+   break;
+ }
+
+ return 0;
+}
 
 /** Calculate the time the specified bytes take to play with the specified sample type */
 pa_usec_t pa_bytes_to_usec(uint64_t length, const pa_sample_spec *spec);
@@ -54,12 +79,41 @@ pa_usec_t pa_bytes_to_usec(uint64_t length, const pa_sample_spec *spec);
 size_t pa_usec_to_bytes(pa_usec_t t, const pa_sample_spec *spec);
 
 /** Return non-zero when the sample type specification is valid */
-int pa_sample_spec_valid(const pa_sample_spec *spec);
+int pa_sample_spec_valid(const pa_sample_spec *spec) {
+ if ( spec == NULL )
+  return 0;
+
+ if ( spec->channels < 1 || spec->channels > ROAR_MAX_CHANNELS )
+  return 0;
+
+ if ( spec->format < 0 || spec->format > PA_SAMPLE_MAX )
+  return 0;
+
+ return 1;
+}
 
 /** Return non-zero when the two sample type specifications match */
-int pa_sample_spec_equal(const pa_sample_spec*a, const pa_sample_spec*b);
+int pa_sample_spec_equal(const pa_sample_spec*a, const pa_sample_spec*b) {
+ if ( a->rate != b->rate )
+  return 0;
+
+ if ( a->channels != b->channels )
+  return 0;
+
+ if ( a->format != b->format )
+  return 0;
+
+ return 1;
+}
 
 /** Return a descriptive string for the specified sample format. \since 0.8 */
+static struct {
+ pa_sample_format_t format;
+ const char * name;
+} _roar_pa_format[] = {
+ {PA_SAMPLE_INVALID, NULL}
+};
+
 const char *pa_sample_format_to_string(pa_sample_format_t f);
 
 /** Parse a sample format text. Inverse of pa_sample_format_to_string() */
