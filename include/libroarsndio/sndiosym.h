cbbe19875c404d4c4ba1282fea828863382bc0ad
phi <phi>
2010-07-10 23:54:41 +0000

    corrected Alexandre Ratchov's copyright

--- include/libroarsndio/sndiosym.h
+++ include/libroarsndio/sndiosym.h
@@ -3,9 +3,25 @@
 /*
  *      Copyright (C) Philipp 'ph3-der-loewe' Schafft - 2009-2010
  *  The code (may) include prototypes and comments (and maybe
- *  other code fragements) from EsounD.
- *  They are mostly copyrighted by Eric B. Mitchell (aka 'Ricdude)
- *  <ericmit@ix.netcom.com>. For more information see AUTHORS.esd.
+ *  other code fragements) from OpenBSD's sndio.
+ *  See 'Copyright for sndio' below for more information on
+ *  code fragments taken from OpenBSD's sndio.
+ *
+ * --- Copyright for sndio ---
+ * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * --- End of Copyright for sndio ---
  *
  *  This file is part of libroaresd a part of RoarAudio,
  *  a cross-platform sound system for both, home and professional use.
@@ -22,8 +38,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this software; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  *  NOTE for everyone want's to change something and send patches:
  *  read README and HACKING! There a addition information on
