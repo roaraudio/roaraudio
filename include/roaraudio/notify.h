1357ebcb1bc01cf2a8235e9a4f8f320a4186931c
phi <phi>
2010-09-07 00:33:44 +0000

    some IDs and macros

--- include/roaraudio/notify.h
+++ include/roaraudio/notify.h
@@ -29,6 +29,32 @@
 #ifndef _ROARAUDIO_NOTIFY_H_
 #define _ROARAUDIO_NOTIFY_H_
 
+#define ROAR_NOTIFY_SPECIAL       0xffffffff
+
+// protocol cmds:
+#define ROAR_NOTIFY_OFFSET_CMD    0x00000000
+#define ROAR_NOTIFY_MASK_CMD      0x000000ff
+
+// event groups (AKA meta events):
+#define ROAR_NOTIFY_OFFSET_EGRP    0x00010000
+#define ROAR_NOTIFY_MASK_EGRP      0x0000ffff
+
+// user defined events:
+#define ROAR_NOTIFY_OFFSET_USER   0x40000000
+#define ROAR_NOTIFY_MASK_CUSER    0x3fffffff
+
+#define ROAR_NOTIFY_X2EVENT(x,offset,mask) ((x) == -1 ? ROAR_NOTIFY_SPECIAL : (((x) & (mask))+(offset)))
+#define ROAR_NOTIFY_EVENT2X(x,offset,mask) (ROAR_NOTIFY_ISX((x),(offset),(mask)) != 1 ? -1 : ((x) - (offset)))
+#define ROAR_NOTIFY_IS_X(x,offset,mask)    ((x) == ROAR_NOTIFY_SPECIAL ? -1 : (((x) | (mask)) - (mask)) == (offset) ? 1 : 0)
+
+#define ROAR_NOTIFY_CMD2EVENT(x)  ROAR_NOTIFY_X2EVENT((x), ROAR_NOTIFY_OFFSET_CMD,  ROAR_NOTIFY_MASK_CMD)
+#define ROAR_NOTIFY_EVENT2CMD(x)  ROAR_NOTIFY_EVENT2X((x), ROAR_NOTIFY_OFFSET_CMD,  ROAR_NOTIFY_MASK_CMD)
+#define ROAR_NOTIFY_IS_CMD(x)     ROAR_NOTIFY_IS_X((x),    ROAR_NOTIFY_OFFSET_CMD,  ROAR_NOTIFY_MASK_CMD)
+
+#define ROAR_NOTIFY_USER2EVENT(x) ROAR_NOTIFY_X2EVENT((x), ROAR_NOTIFY_OFFSET_USER, ROAR_NOTIFY_MASK_USER)
+#define ROAR_NOTIFY_EVENT2USER(x) ROAR_NOTIFY_EVENT2X((x), ROAR_NOTIFY_OFFSET_USER, ROAR_NOTIFY_MASK_USER)
+#define ROAR_NOTIFY_IS_USER(x)    ROAR_NOTIFY_IS_X((x),    ROAR_NOTIFY_OFFSET_USER, ROAR_NOTIFY_MASK_USER)
+
 #endif
 
 //ll
